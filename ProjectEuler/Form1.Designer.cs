aabfb709384d38c9f934de681c5b8bf8ff2f71f1
TajaAgus <104028075+TajaAgus@users.noreply.github.com>
2022-06-16 13:26:23 -0300

    first commit

---
+++ ProjectEuler/Form1.Designer.cs
@@ -0,0 +1,345 @@
+﻿namespace ProjectEuler
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.button5 = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.label7 = new System.Windows.Forms.Label();
+            this.button8 = new System.Windows.Forms.Button();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.button9 = new System.Windows.Forms.Button();
+            this.button10 = new System.Windows.Forms.Button();
+            this.label10 = new System.Windows.Forms.Label();
+            this.button11 = new System.Windows.Forms.Button();
+            this.label11 = new System.Windows.Forms.Label();
+            this.button12 = new System.Windows.Forms.Button();
+            this.label12 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(25, 11);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(60, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Ejercicio 1";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(91, 7);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(25, 40);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(60, 15);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Ejercicio 2";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(91, 36);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(25, 69);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(60, 15);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Ejercicio 3";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(91, 65);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(91, 94);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 7;
+            this.button4.Text = "button4";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(25, 98);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(60, 15);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Ejercicio 4";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(25, 126);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(60, 15);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Ejercicio 5";
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(91, 122);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.TabIndex = 10;
+            this.button5.Text = "button5";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(25, 155);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(60, 15);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "Ejercicio 6";
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(91, 151);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(75, 23);
+            this.button6.TabIndex = 12;
+            this.button6.Text = "button6";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(91, 180);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(75, 23);
+            this.button7.TabIndex = 24;
+            this.button7.Text = "button7";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(25, 328);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(66, 15);
+            this.label7.TabIndex = 23;
+            this.label7.Text = "Ejercicio 12";
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(91, 209);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(75, 23);
+            this.button8.TabIndex = 22;
+            this.button8.Text = "button8";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(25, 299);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(66, 15);
+            this.label8.TabIndex = 21;
+            this.label8.Text = "Ejercicio 11";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(25, 270);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(66, 15);
+            this.label9.TabIndex = 20;
+            this.label9.Text = "Ejercicio 10";
+            // 
+            // button9
+            // 
+            this.button9.Location = new System.Drawing.Point(91, 238);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(75, 23);
+            this.button9.TabIndex = 19;
+            this.button9.Text = "button9";
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // button10
+            // 
+            this.button10.Location = new System.Drawing.Point(91, 266);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(75, 23);
+            this.button10.TabIndex = 18;
+            this.button10.Text = "button10";
+            this.button10.UseVisualStyleBackColor = true;
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(25, 242);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(60, 15);
+            this.label10.TabIndex = 17;
+            this.label10.Text = "Ejercicio 9";
+            // 
+            // button11
+            // 
+            this.button11.Location = new System.Drawing.Point(91, 295);
+            this.button11.Name = "button11";
+            this.button11.Size = new System.Drawing.Size(75, 23);
+            this.button11.TabIndex = 16;
+            this.button11.Text = "button11";
+            this.button11.UseVisualStyleBackColor = true;
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(25, 213);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(60, 15);
+            this.label11.TabIndex = 15;
+            this.label11.Text = "Ejercicio 8";
+            // 
+            // button12
+            // 
+            this.button12.Location = new System.Drawing.Point(91, 324);
+            this.button12.Name = "button12";
+            this.button12.Size = new System.Drawing.Size(75, 23);
+            this.button12.TabIndex = 14;
+            this.button12.Text = "button12";
+            this.button12.UseVisualStyleBackColor = true;
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(25, 184);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(60, 15);
+            this.label12.TabIndex = 13;
+            this.label12.Text = "Ejercicio 7";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button10);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.button11);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.button12);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Button button11;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Button button12;
+        private System.Windows.Forms.Label label12;
+    }
+}
---
+++ ProjectEuler/Form1.cs
@@ -0,0 +1,255 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProjectEuler
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //Ejercicio 1
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int result3 = 0;
+            int result5 = 0;
+            int total = 0;
+            for (int i = 1; result3 < 1000; i++)
+            {
+                result3 = i * 3;
+                result5 = i * 5;
+                if (result3 < 1000 && result3 % 5 != 0)
+                {
+                    total += result3;
+                }
+                if (result5 < 1000)
+                { 
+                    total += result5;
+                }
+            }
+            MessageBox.Show(total.ToString());
+        }
+
+        //Ejercicio 2
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int number1 = 1;
+            int number2 = 2;
+            int flag = 0;
+            int result = 0;
+            while (true)
+            {
+                if (number1 < 4000000 && number1 % 2 == 0)
+                {
+                    result += number1;
+                }
+                if (number2 < 4000000 && number2 % 2 == 0)
+                {
+                    result += number2;
+                }
+                flag = number1 + number2;
+                number1 = flag;
+                flag = number1 + number2;
+                number2 = flag;
+
+                if (number1 >= 4000000 && number2 >= 4000000)
+                {
+                    break;
+                }
+            }
+            MessageBox.Show(result.ToString());
+        }
+
+        //Ejercicio 3
+        private void button3_Click(object sender, EventArgs e)
+        {
+            int primeFactor = 0;
+            bool isPrime = true;
+            long result = 1;
+
+            for (int i = 2; true; i++)
+            {
+                if (600851475143 % i  == 0 )
+                {
+                    //for (int j = 2; j < i && isPrime; j++)
+                    //{
+                    //    if (i % j == 0)
+                    //    {
+                    //        isPrime = false;
+                    //    }
+                    //}
+                    //if (isPrime)
+                    //{
+                        result *= i;
+                        if (result == 600851475143)
+                        {
+                            primeFactor = i;
+                            MessageBox.Show(primeFactor.ToString());
+                            break;
+                        }
+                    //}
+                }
+            }
+        }
+
+        //Ejercicio 4
+        private void button4_Click(object sender, EventArgs e)
+        {
+            int palindromicNumber = 0;
+            int product = 0;
+            int product2 = 0;
+            int number = 0;
+            int finalNumber = 0;
+            string numberString = "";
+            bool stop;
+
+            for (int i = 100; i < 1000; i++)
+            {
+                for (int j = i; j < 1000; j++)
+                {
+                    numberString = "";
+                    stop = false;
+                    product = i * j;
+                    product2 = product;
+
+                    while (!stop)
+                    {
+                        if (product2 != 0 && product2 >= 1)
+                        {
+                            number = product2 % 10;
+                            product2 = product2 / 10;
+                            if (number >= 1 || number == 0)
+                            {
+                                numberString += "" + number;
+                            }
+                        }
+                        else stop = true;
+                    }
+                    finalNumber = Int32.Parse(numberString);
+                    if (finalNumber == product && finalNumber > palindromicNumber)
+                    {
+                        palindromicNumber = product;
+                    }
+                }
+            } 
+            MessageBox.Show(palindromicNumber.ToString());
+        }
+
+        //Ejercicio 5
+        private void button5_Click(object sender, EventArgs e)
+        {
+            long number = 20;
+            double number2 = 0;
+            for (double i = 2; i <= 20; i++)
+            {
+                number2 = number / i;
+                if (number2 % 1 != 0)
+                {
+                    i = 2;
+                    number++;
+                }
+            }
+            MessageBox.Show(number.ToString());
+        }
+
+        //Ejercicio 6
+        private void button6_Click(object sender, EventArgs e)
+        {
+            int sumOfTheSquares = 0;
+            int squareOfTheSum = 0;
+            int result = 0;
+            for (int i = 1; i <= 100; i++)
+            {
+                sumOfTheSquares += i * i;
+            }
+            for (int i = 1; i <= 100; i++)
+            {
+                squareOfTheSum += i; 
+            }
+            squareOfTheSum *= squareOfTheSum;
+            result = squareOfTheSum - sumOfTheSquares;
+            MessageBox.Show(result.ToString());
+        }
+
+        //Ejercicio 7
+        private void button7_Click(object sender, EventArgs e)
+        {
+            bool isPrime = true;
+            int primeCounter = 0;
+            for (int i = 2; true; i++)
+            {
+                isPrime = true;
+                for (int j = 2; j < i && isPrime; j++)
+                {
+                    if (i % j == 0)
+                    {
+                        isPrime = false;
+                    }
+                }
+                if (isPrime)
+                {
+                    primeCounter++;
+                }
+                if (primeCounter == 10001)
+                {
+                    MessageBox.Show(i.ToString());
+                    break;
+                }
+            }
+        }
+        
+        //Ejercicio 8
+        private void button8_Click(object sender, EventArgs e)
+        {
+            string numbersString = "731671765313306249192251196744265747423553491949349698352031277450632623957831801698480" + 
+                "18694788518438586156078911294949545950173795833195285320880551112540698747158523863050715693290963295227443043557668966489504" + 
+                "4524452316173185640309871112172238311362229893423380308135336276614282806444486645238749303589072962904915604407723907138105158593079" +
+                "6086670172427121883998797908792274921901699720888093776657273330010533678812202354218097512545405947522435258490771167055601360483958" +
+                "6446706324415722155397536978179778461740649551492908625693219784686224828397224137565705605749026140797296865241453510047482166370484" +
+                "4031998900088952434506585412275886668811642717147992444292823086346567481391912316282458617866458359124566529476545682848912883142607" +
+                "6900422421902267105562632111110937054421750694165896040807198403850962455444362981230987879927244284909188845801561660979191338754992" +
+                "005240636899125607176060588611646710940507754100225698315520005593572972571636269561882670428252483600823257530420752963450";
+            long product = 1;
+            long result = 0;
+            int index = 0;
+
+            for (int i = 0; true; i++)
+            {
+                for (int j = 0; j <= 12 && product != 0; j++)
+                {
+                    index = i + j;
+                    if (index <= 999)
+                    {
+                        product *= (int)Char.GetNumericValue((char)numbersString[index]);
+                    }
+                    else goto stop;
+                }
+                if (product != 0 && product > result)
+                {
+                    result = product;
+                }
+                product = 1;
+            }
+            stop:
+                MessageBox.Show(result.ToString());
+        }
+
+        //Ejercicio 9
+        // a * a + b * b = c * c 
+        // a = p * p - q * q
+        // b = 2 * p * q
+        // c = p * p + q * q
+        private void button9_Click(object sender, EventArgs e)
+        {
+        }
+    }
+}
